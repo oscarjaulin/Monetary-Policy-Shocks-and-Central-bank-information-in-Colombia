9c186ad9ec45814c67cff7160c5dffcd047b716a
Oscar Jaulín Méndez <oscarjaulin@gmail.com>
2026-02-23 12:56:51 -0500

    :rocket: Specification that works
    The specification that allows us to obtain important results. These are the resutls:
    1. When using the BEI for orthogonalization, MP reduces CPI and the CBI is the one with the price puzzle.
    2. When using the COLCAP, it is not the case. It seems that MP is the cause of the  price puzzle, while CBI kind of corrects it.

--- R/Programs/Compute_IRF.R
+++ R/Programs/Compute_IRF.R
@@ -5,6 +5,7 @@ library(readxl)
 library(boot)
 require('ggplot2')
 library(BVAR)
+library(bsvars)
 
 #Run some required funcitions
 source('C:/Users/ojaulime/OneDrive - Banco de la República/Documents/Research/MP transmission in Colombia/Monetary-Policy-Shocks-and-Central-bank-information-in-Colombia/R/Programs/create_lags.r')
@@ -20,11 +21,11 @@ DATA      <- read_excel('DATA.xlsx')
 #1). Dependent Variables
 Var_names  = c('GDP','INT','CPI','EXC')
 Var_names  = c('CPI')
-Var_Cont   = c('GDP','INT','CPI','EXC','CDS')
-Var_Cont   = c('GDP','INT','CPI','EXC')
+#Var_Cont   = c('GDP','CPI','INT','EXC','CDS')
+Var_Cont   = c('GDP','CPI','INT','CDS')
 #2). Monetary policy shock proxies
 MP_shocks  = c('ShockBloom','ShockOISL','ShockBR')
-MP_shocks  = c('ShockOISL','MP_OIS_BEI','CBI_OIS_BEI')
+MP_shocks  = c('ShockBloom','MP_bloo_BEI','CBI_bloo_BEI')
 
 #3) High frequency shocks in
 HF_Shocks  = c('Shock CDs','Shock TRM')
@@ -48,8 +49,8 @@ for(ss in MP_shocks){
 
 # Extract the CDS component from the shocks
 DATA_Cop= DATA
-ss = MP_shocks[1]
-Data_Risk = create_lagged_df(DATA_Cop, vars = c('INT','EXC','CPI','GDP'), h = 1)
+ss = MP_shocks[3]
+Data_Risk = create_lagged_df(DATA_Cop, vars = c('INT','CDS','CPI','GDP'), h = 1)
 Data_Shock= merge(DATA_Cop[,c('Date',ss)],Data_Risk,by = 'Date')
 Data_Shock= Data_Shock[complete.cases(Data_Shock),]
 fml       = as.formula(paste0(ss,'~ ',paste0(colnames(Data_Risk[,2:ncol(Data_Risk),drop=F]),collapse = '+')))
@@ -64,7 +65,9 @@ Data_Anal  <- DATA_Cop[,c('Date',unique(c(Var_names)),shock_anal)]
 Data_Anal  <- Data_Anal[complete.cases(Data_Anal),]
 source('C:/Users/ojaulime/OneDrive - Banco de la República/Documents/Research/MP transmission in Colombia/Monetary-Policy-Shocks-and-Central-bank-information-in-Colombia/R/Programs/IRF_Mean.r')
 
-## Estimating a BVAR
+#----------------------------------------------
+# Estimation of a BVAR using the package bvar
+#----------------------------------------------
 pLags      = 13
 nSteps     = 60
 Confidence = 0.68
@@ -150,6 +153,44 @@ for(i in 1:length(VarNames2)) {
 }
 
 
+#----------------------------------------------
+# Estimation of a BVAR using the package bsvars
+#----------------------------------------------
+pLags      = 6
+nSteps     = 60
+Confidence = 1-conf1
+Var_shock  = c('ShockBloom')
+Var_shock  = c('CBI_bloomberg')
+VarNames2  = c(Var_shock,Var_Cont)
+#Var_shock  = c('INT')
+#VarNames2  = Var_Cont
+year_ini  = substr(DATA$Date,1,4)[1]
+month_ini = as.numeric(substr(DATA$Date,6,7)[1])
+Data_CGV  = matrix(as.numeric(as.matrix(DATA[,VarNames2])),nrow(DATA),length(VarNames2))
+colnames(Data_CGV) = VarNames2
+#X  <- na.omit(df[, c("Y","pi","i")])
+X  <- na.omit(Data_CGV[, c(VarNames2)])
+
+# 2) Especificación BVAR homocedástico (BVAR con Minnesota)
+p <- pLags
+stationary_vec <- c(FALSE,rep(TRUE, ncol(X)-1))  # ajusta según tus series
+
+spec <- specify_bsvar$new(
+  data       = as.matrix(X),
+  p          = p,
+  stationary = stationary_vec,
+)
+
+# 3) Estimación (burn-in + cadena principal)
+fit_burn <- estimate(spec,     S = 2000,  thin = 1)
+fit_bvar <- estimate(fit_burn, S = 20000, thin = 1)
+
+# 4) Resumen
+summary(fit_bvar)
+
+
+irfs <- compute_impulse_responses(fit_bvar, horizon = nSteps)
+plot(irfs,probability = Confidence)
 
 
 
